042b2e1efb8864dacbb647b73eb14939fa0a54da
LibreSys <goose.goosev.2000@gmail.com>
2025-11-21 20:56:39 +0700

    Removed error

--- entry/entry.asm
+++ entry/entry.asm
@@ -1,3 +1,6 @@
+; kernel.asm — 32-bit start -> switch to long mode -> call kmain (64-bit)
+; Assemble with: nasm -f elf64 kernel.asm -o build/kernel.asm.o
+
 [BITS 32]
 
 section .text
@@ -23,20 +26,10 @@ mb2_start:
     ; checksum = -(magic + arch + length)  (выражение ниже даёт 32-bit)
     dd 0x100000000 - (0xE85250D6 + 0x00000000 + (mb2_end - mb2_start))
 
-    ; --- Framebuffer (header) tag: request preferred mode ---
-    align 8
-    dw 5        ; tag type = 5 (framebuffer request in header)
-    dw 0        ; flags (0 = required; if you want optional set bit0)
-    dd 20       ; size of this tag (header + payload: 2+2+4 + 3*4 = 20)
-    dd 1920     ; width (px)  <-- поменяйте на нужную
-    dd 1080      ; height (px) <-- поменяйте на нужную
-    dd 32       ; bpp (bits per pixel) <-- 32 обычно
-
-    ; --- End tag ---
-    align 8
-    dw 0
-    dw 0
-    dd 8
+    ; --- минимальный список тэгов: конец (type=0, flags=0, size=8) ---
+    dw 0x0000                  ; tag type = 0 (end)
+    dw 0x0000                  ; flags = 0
+    dd 0x00000008              ; size = 8
 
 mb2_end:
 
@@ -45,10 +38,7 @@ global start
 extern kernel_main        ; 64-bit C entry point (link with -m64 objects)
 
 start:
-    cli  
-    
-    mov edi, ebx            ; EBX — указатель на multiboot_info
-    add edi, 8              ; пропускаем magic, flags, checksum (первые 8 байт)
+    cli                     ; отключаем прерывания
 
     ; --- Загружаем GDT (должен содержать 64-bit code selector в 0x08) ---
     lgdt [gdt_desc]
@@ -92,7 +82,6 @@ long_mode_entry:
     and rsp, -16
 
     ; вызов 64-битного kmain (собранного с -m64)
-    mov rdi, rbx
     call kernel_main
 
 .hang64:
@@ -121,65 +110,33 @@ gdt_desc:
 ; -----------------------------------------------------------------------
 section .bss
 align 16
-stack64_bottom:
-    resb 65536           ; резервируем 64 KiB под стек
+resb 16384
 stack64_top:
 
 ; -----------------------------------------------------------------------
-; Identity-map 0..4GiB (2MiB pages) через PML4->PDPT->PD0..PD3
-; - PML4[0] -> PDPT
-; - PDPT[0..3] -> PD0..PD3 (каждый PD покрывает 1GiB)
-; Флаги: PD entries = 0x087 (Present|RW|US|PS(2MiB))
-; PML4/PDPT entries = 0x007 (Present|RW|US)
+; Простая identity map: PML4 -> PDPT -> PD (512 x 2MiB = 1GiB)
+; Используем выровненные таблицы, создаём 512 PDE, каждое значение = base_of_2MiB_chunk + flags
+; Флаги: Present | RW | PS(2MiB) = 0x83
+; PML4 entry and PDPT entry: Present | RW = 0x03
 ; -----------------------------------------------------------------------
 section .data
 align 4096
 pml4_table:
-    dq pdpt_table + 0x007    ; PML4[0] -> PDPT (flags in low bits)
+    dq pdpt_table + 0x007    ; Present | RW | US
 
 align 4096
 pdpt_table:
-    dq pd_table0 + 0x007     ; PDPT[0] -> PD0 (0..1GiB)
-    dq pd_table1 + 0x007     ; PDPT[1] -> PD1 (1..2GiB)
-    dq pd_table2 + 0x007     ; PDPT[2] -> PD2 (2..3GiB)
-    dq pd_table3 + 0x007     ; PDPT[3] -> PD3 (3..4GiB)
-    ; остальные записи нулевые (по умолчанию)
+    dq pd_table + 0x007      ; Present | RW | US
 
-; PD0: maps 0x0000_0000 .. 0x3FF_FFFF (1 GiB)
 align 4096
-pd_table0:
+pd_table:
 %assign j 0
 %rep 512
+    ; addr = j * 0x200000, flags = Present | RW | US | PS(2MiB) = 0x87
     dq j * 0x200000 + 0x087
 %assign j j + 1
 %endrep
 
-; PD1: maps 0x4000_0000 .. 0x7FF_FFFF
-align 4096
-pd_table1:
-%assign j 512
-%rep 512
-    dq j * 0x200000 + 0x087
-%assign j j + 1
-%endrep
-
-; PD2: maps 0x8000_0000 .. 0xBFF_FFFF
-align 4096
-pd_table2:
-%assign j 1024
-%rep 512
-    dq j * 0x200000 + 0x087
-%assign j j + 1
-%endrep
-
-; PD3: maps 0xC000_0000 .. 0xFFF_FFFF  (сюда попадает 0xFD000000)
-align 4096
-pd_table3:
-%assign j 1536
-%rep 512
-    dq j * 0x200000 + 0x087
-%assign j j + 1
-%endrep
 
 ; -----------------------------------------------------------------------
 ; Конец
