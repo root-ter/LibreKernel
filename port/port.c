d4475b17beaa3bec5141ff0701b666d1b4e61e1f
LibreSys <goose.goosev.2000@gmail.com>
2025-11-21 20:58:48 +0700

    IO

---
+++ port/port.c
@@ -0,0 +1,26 @@
+#include "port.h"
+#include <stdint.h>
+
+uint8_t inb(uint16_t port)
+{
+    uint8_t ret;
+    asm volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+void outb(uint16_t port, uint8_t data)
+{
+    asm volatile("outb %0, %1" : : "a"(data), "Nd"(port));
+}
+
+uint16_t inw(uint16_t port)
+{
+    uint16_t ret;
+    asm volatile("inw %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+void outw(uint16_t port, uint16_t data)
+{
+    asm volatile("outw %0, %1" : : "a"(data), "Nd"(port));
+}
